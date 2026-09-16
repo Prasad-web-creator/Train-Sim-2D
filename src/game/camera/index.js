f6301bbdb9e7d659828c2278dcb8a93d59a28048
MagendraPrasad <magendraprasad84@gmail.com>
2026-09-16 20:10:24 +0530

    Delete src directory

--- src/game/camera/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * index.js
- * Public module exports for the TrainSim camera subsystem.
- */
-
-export { CameraSystem, cons_CAMERA_MODES, cons_ZOOM_PRESETS } from './CameraSystem.js';
-export { WorldCamera } from './WorldCamera.js';
-export { ParallaxCamera } from './ParallaxCamera.js';
--- src/game/core/Camera.js
+++
@@ -1,9 +0,0 @@
-/**
- * Camera.js
- * Backward compatibility adapter exporting WorldCamera.
- */
-
-import { WorldCamera } from '../camera/WorldCamera.js';
-
-export { WorldCamera as Camera };
-export default WorldCamera;
